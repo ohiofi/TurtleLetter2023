98822d8a941c81cde728dab87880f2edbce15b4d
CampbellToeOfficial <147730618+CampbellToeOfficial@users.noreply.github.com>
2023-10-18 14:26:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from turtle import *
 # uppercase letters go up here
+from turtle import *
 
+def drawC():
+  goto(0, 0)
+  penup()
+  backward(50)
+  left(90)
+  forward(25)
+  forward(20)
+  pendown()
+  
+  for i in range(17):
+    left(20)
+    forward(20)
+
+def drawL():
+  penup()
+  goto(0, -20)
+  left(20)
+  pendown()
+  forward(105)
+  penup()
+  goto(0, -20)
+  right(90)
+  pendown()
+  forward(75)
+
+drawC()
+drawL()
 
 
 

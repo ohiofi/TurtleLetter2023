70ec88ae97fe54633fcf35d3dc7e6da0238e70a4
ywouch <148380702+ywouch@users.noreply.github.com>
2023-10-18 14:23:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from turtle import *
 # uppercase letters go up here
-
-
-
-
-
-
-
+def upperT():
+    turtle.pendown()
+    turtle.forward(95)
+    turtle.penup()
+    turtle.right(180)
+    turtle.forward(47.5)
+    turtle.left(90)
+    turtle.pendown()
+    turtle.forward(100)
+    turtle.penup()
+    turtle.right(180)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    return
+def upperJ():
+    turtle.pendown()
+    turtle.right(90)
+    turtle.forward(75)
+    turtle.penup()
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.pendown()
+    turtle.right(270)
+    turtle.circle(25,180)
+    return
 # lowercase letters down here
 
 

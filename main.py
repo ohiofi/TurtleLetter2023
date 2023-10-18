49e4e73c1d4eb766c56107f196ec731c241193d0
AM1222sc <115640692+AM1222sc@users.noreply.github.com>
2023-10-18 09:00:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from turtle import *
 # uppercase letters go up here
+from turtle import *
+def A():
+  
+  penup()
+  pencolor('#111166')
+  goto(-200, 100)
+  pendown()
+  goto(-100, -100)
+  goto(-150, 0)
+  goto(-250, 0)
+  goto(-200, 100)
+  goto(-300, -100)
+  penup()
+def S():
+  goto(200, 100)
+  pendown()
+  goto(100, 100)
+  goto(100, 0)
+  goto(200, 0)
+  goto(200, -100)
+  goto(100, -100)
+A()
+S()
 
 
 

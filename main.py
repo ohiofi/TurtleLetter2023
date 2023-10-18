fe42527ec8c41ca9527afde5e16d72b061ccbb15
eshasiddavarapu <148379418+eshasiddavarapu@users.noreply.github.com>
2023-10-18 14:22:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from turtle import *
 # uppercase letters go up here
+import turtle
+
+
+t = turtle.Turtle()
+t.penup()
+t.goto(0, 0)
+t.pendown()
+t.forward(-45)
+t.setheading(90)
+t.forward(45)
+t.right(90)
+t.forward(45)
+t.forward(-45)
+t.setheading(90)
+t.forward(45)
+t.right(90)
+t.forward(45)
+t.hideturtle()
+
+import turtle
+t = turtle.Turtle()
+t.penup()
+t.goto(6, 0)
+t.pendown()
+t.forward(40)
+t.circle(25, 180)
+t.circle(-25, 180)
+t.forward(40)
 
 
 

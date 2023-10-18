dd25fdfb8f4116b5e86db00460253e533dad0f08
RoshanAyanampudi <148380755+RoshanAyanampudi@users.noreply.github.com>
2023-10-18 14:22:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from turtle import *
 # uppercase letters go up here
+def drawA():
+  turtle.pendown()
+  turtle.forward(200)
+  turtle.right(150)
+  turtle.forward(200)
+  turtle.left(180)
+  turtle.forward(100)
+  turtle.left(75)
+  turtle.forward(60)
 
 
+def drawR():
+  turtle.right(90)
+  turtle.forward(50)
+  turtle.right(180)
+  turtle.forward(200)
+  turtle.right(90)
+  for i in range(90):
+    turtle.right(2)
+    turtle.forward(2)
+  turtle.left(120)
+  turtle.forward(100)
 
 
 

330cbad6557f0a51e8a05c7f9277d6a04aa815b1
Rydxrr <148351157+Rydxrr@users.noreply.github.com>
2023-10-18 09:15:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from turtle import *
 # uppercase letters go up here
+def drawR():
+  penup()
+  goto(-300, -100)
+  pendown()
+  setheading(90)
+  forward(250)
+  right(90)
+  forward(100)
+  right(90)
+  forward(100)
+  right(90)
+  forward(100)
+  left(125)
+  forward(180.27)
 
-
-
+def drawW():
+  penup()
+  goto(-150, 100)
+  pendown()
+  setheading(-65)
+  forward(254)
+  left(130)
+  forward(254)
+  right(130)
+  forward(254)
+  left(130)
+  forward(254)
 
 
 
